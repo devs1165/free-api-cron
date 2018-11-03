092cbba0b8969066e84b82552d467ea7ed5463b9
devil1165 <devkant@getambee.com>
2018-11-03 20:21:30 +0530

    fetch api fixed

--- cronServer.js
+++ cronServer.js
@@ -5,7 +5,7 @@ var request = require('request');
 new CronJob('* */8 * * *', function() {
  	console.log('You will see this message every second');
 	
-	request('http://52.66.212.208:3000', function (error, response, body) {
+	request('http://52.66.212.208:3000/feedreading', function (error, response, body) {
 
 	  console.log('error:', error); // Print the error if one occurred
 	  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
